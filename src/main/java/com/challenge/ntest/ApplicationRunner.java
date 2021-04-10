b9be14209d6b039c0bba8e7f0e68d8dd08e61862
Fabio Maciel <6810827+fabiomaciel@users.noreply.github.com>
2021-04-10 13:34:13 -0300

    feat: add initial boilerplate project

---
+++ src/main/java/com/challenge/ntest/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package com.challenge.ntest;
+
+import java.util.Scanner;
+
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while(input.hasNext()){
+            System.out.println(input.nextLine());
+        }
+    }
+}
