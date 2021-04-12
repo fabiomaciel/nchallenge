7065f04fbc7b7511b1aae1838c3f5f1b6b67d827
Fábio Maciel <6810827+fabiomaciel@users.noreply.github.com>
2021-04-12 19:29:43 -0300

    Update ApplicationRunner.java

--- src/main/java/com/challenge/ntest/ApplicationRunner.java
+++ src/main/java/com/challenge/ntest/ApplicationRunner.java
@@ -52,3 +52,149 @@ public class ApplicationRunner {
         System.out.println(transactions);
     }
 }
+
+
+package com.challenge.ntest.domain.services;
+
+import com.challenge.ntest.domain.models.Account;
+import com.challenge.ntest.domain.models.State;
+import com.challenge.ntest.domain.models.Transaction;
+import com.challenge.ntest.domain.models.ViolationType;
+import com.challenge.ntest.domain.models.Violations;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+public class StateManager {
+
+    LinkedList<State> history;
+
+    public StateManager() {
+        this.history = new LinkedList<>();
+    }
+
+    public List<State> getHistory() {
+        return history;
+    }
+
+    public State getCurrent() {
+        if (history.isEmpty()) return null;
+        return history.getLast();
+    }
+
+    public Account getAccount(){
+        return Optional.ofNullable(getCurrent())
+                .map(State::getAccount)
+                .orElse(null);
+    }
+
+    public State performOperation(Account account) {
+        Violations violations = new Violations();
+
+        if (Objects.nonNull(getAccount())) {
+            violations.add(ViolationType.ACCOUNT_ALREADY_INITIALIZED);
+            account = getAccount();
+        }
+
+        return addState(account, violations);
+
+    }
+
+    public State performOperation(Transaction transaction){
+        Violations violations = new Violations();
+        Account account = getAccount();
+
+        if(Objects.isNull(account)){
+            violations.add(ViolationType.ACCOUNT_NOT_INITIALIZED);
+            return addState(null, violations);
+        }
+
+        if(!account.isActiveCard()){
+            violations.add(ViolationType.CARD_NOT_ACTIVE);
+            return addState(account, violations);
+        }
+
+        long spare = account.getAvailableLimit() - transaction.getAmount();
+        if(spare < 0){
+            violations.add(ViolationType.INSUFFICIENT_LIMIT);
+            return addState(account, violations);
+        }else{
+            account = new Account(true, spare);
+        }
+
+        return addState(account, violations);
+    }
+
+    //TODO: rename this method
+    public State addState(Account account, Violations violations) {
+        return addState(new State(account, violations));
+    }
+
+    public State addState(State state){
+        history.add(state);
+        return state;
+    }
+
+
+}
+
+package com.challenge.ntest.domain.models;
+
+public enum ViolationType {
+    ACCOUNT_ALREADY_INITIALIZED("account-already-initialized"),
+    ACCOUNT_NOT_INITIALIZED("account-not-initialized"),
+    CARD_NOT_ACTIVE("card-not-active"),
+    INSUFFICIENT_LIMIT("insufficient-limit");
+
+    private final String name;
+
+    ViolationType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+package com.challenge.ntest.domain.models;
+
+public class State {
+
+    private Account account;
+    private Violations violations;
+
+    public State() {
+        this.violations = new Violations();
+    }
+
+    public State(Account account) {
+        this(account, new Violations());
+    }
+
+    public State(Account account, Violations violations) {
+        this.violations = violations;
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Violations getViolations() {
+        return violations;
+    }
+
+    public void setViolations(Violations violations) {
+        this.violations = violations;
+    }
+
+    public void addViolation(String violation) {
+        this.violations.add(violation);
+    }
+}
